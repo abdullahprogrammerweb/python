f801691224ac62bdfb224d6895fa7c0fd0254e32
Abdullah <114979134+abdullahprogrammerweb@users.noreply.github.com>
2024-04-29 20:12:47 +0000

    first day python

---
+++ hello.py
@@ -0,0 +1,40 @@
+import sys
+print('python version is here',sys.version)
+print('abdullah')
+
+# Identation in python
+if(5>2):
+ print('True')
+
+# give an error
+# if(5>2):
+# print('True')
+ 
+
+# working corrected way
+ if(5>2):
+  print('ture')
+  print('working')
+
+# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error
+# GETTING AN ERROR
+#   if(5>2):
+#    print('true')
+#     print('error happend')
+  
+
+# variable in python
+# Python has no command for declaring a variable.
+ x = 5
+ y = 'hello'
+print(x,y)
+
+# comment in python
+# Python has commenting capability for the purpose of in-code documentation.
+# This is an commend
+
+"""
+This is a comment
+written in
+more than just one line
+"""
